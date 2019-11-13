ec8befc2413c7ef4a923b897c3d94353ef1c9d0a
BykovIA <igor.bykov.96@bk.ru>
2019-11-13 22:29:54 +0500

    all code refactoring 13.11.2019

---
+++ src/main/java/ru/house/manager/controller/PageController.java
@@ -0,0 +1,51 @@
+package ru.house.manager.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+
+import ru.house.manager.EntityDB.Users;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import ru.house.manager.serviceDB.UsersService;
+
+@Controller
+public class PageController {
+
+
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String getOrderPage(Model model) {
+        return "loginForm";
+    }
+
+    @RequestMapping(value = "/user-registration", method=RequestMethod.GET)
+    public String addNewOrderPage() {
+        return "userRegistraionForm";
+    }
+
+    @RequestMapping(value="/user-registration", method=RequestMethod.POST)
+    public String addNewOrder(@RequestParam(value="firstName") String firstName, @RequestParam(value="lastName") String lastName, @RequestParam(value="fatherName") String fatherName, @RequestParam(value="phoneNumb") String phoneNumber, @RequestParam(value="eMail") String eMail, @RequestParam(value="roomNumb") String roomNumber) throws UnsupportedEncodingException, SQLException {
+
+        UsersService usersService = new UsersService();
+        Users user = new Users();
+        user.setFirstName(new String(firstName.getBytes("ISO-8859-1"), "UTF-8"));
+        user.setLastName(new String(lastName.getBytes("ISO-8859-1"), "UTF-8"));
+        user.setFatherName(new String(fatherName.getBytes("ISO-8859-1"), "UTF-8"));
+        user.setPhoneNumber(new String(phoneNumber.getBytes("ISO-8859-1"), "UTF-8"));
+        user.seteMail(new String(eMail.getBytes("ISO-8859-1"), "UTF-8"));
+        user.setRoomNumber(new String(roomNumber.getBytes("ISO-8859-1"), "UTF-8"));
+
+        try {
+            usersService.add(user);
+        } catch (SQLException e ) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+}
